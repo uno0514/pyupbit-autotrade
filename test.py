d518e230d2a39c235ae883a74f84a4a336cad884
uno0514 <87837648+uno0514@users.noreply.github.com>
2021-07-23 09:54:24 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ access = "your-access"          # 본인 값으로 변경
 secret = "your-secret"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
-print(upbit.get_balance("KRW-BTC"))     # KRW-BTC 조회
+print(upbit.get_balance("KRW-AXS"))     # KRW-BTC 조회
 print(upbit.get_balance("KRW"))         # 보유 현금 조회
